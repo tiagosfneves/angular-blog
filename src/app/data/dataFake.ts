2f2a344ab2a9d436f57df4f80f4fac10a0112865
Tiago Lenovo <tiagonevestec@gmail.com>
2023-06-03 02:24:30 -0300

    Add theme page, Folder Img and Photos, Favicon, contents

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title":"Título da Notícia-01",
-    "description":"Primeira mão",
-    "photoCover":"https://www.institutioner.dk/img/img_container.png"
+    "title":"Da CLT ao CNPJ!",
+    "description":"Olhando para trás vendo toda minha tragetoria até aqui vejo o quanto superei meus medos e inseguranças, saindo de uma mentalidade de escassez, mediocre e limitada para planejar um negócio do zero, sem estrutura, sem equipamentos, sem espaço, sem nome de empresa, sem cliente, mas com muita vontade de mudança e apoio do meu esposo e família...",
+    "photoCover":"../../../assets/Img/img-01.png"
   },
   {
     "id":"2",
-    "title":"Título da Notícia-02",
-    "description":"Primeira mão e mais",
-    "photoCover":"https://www.institutioner.dk/img/img_container.png"
+    "title":"O primeiro Empreendimento é inesquecível",
+    "description":"É bom demais tirar o plano do papel, após a minha formação em Designer e o desenvolvimento do meu portfolio...",
+    "photoCover":"../../../assets/Img/img-02.png"
+  },
+  {
+    "id":"3",
+    "title":"Quando mudar para um espaço melhor?",
+    "description":"Após um ano de empenho e dedicação, aqui estou eu no meu novo espaço, feliz e entusiasmada com tudo que vem acontecendo...",
+    "photoCover":"../../../assets/Img/img-03.png"
+  },
+  {
+    "id":"4",
+    "title":"Agora eu ensino mulheres a mudarem a vida de outras mulheres!",
+    "description":"Como é incrível levar transformação para as vidas das mulheres mudando seus olhares e auto-estima. E melhor ainda é...",
+    "photoCover":"../../../assets/Img/img-04.png"
   }
 ]
